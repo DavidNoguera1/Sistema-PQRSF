e800fb4442c155d318d1234f8cdc6014e1eeb205
David N <davidnoguera0912@gmail.com>
2024-04-30 22:34:06 -0500

    Index y Registro

--- Base de datos/dbsistema.sql
+++ Base de datos/dbsistema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS dbsistema;
-CREATE DATABASE dbsistema;
+CREATE DATABASE dbsistema CHARACTER SET utf8mb4;
 USE dbsistema;
 
 /* .............Creacion de tablas............ */
@@ -8,6 +8,7 @@ USE dbsistema;
 CREATE TABLE usuarios (
     IdUsuario INT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
     Cedula VARCHAR(20) UNIQUE NOT NULL,
     Correo VARCHAR(100) UNIQUE NOT NULL,
     Contrasena VARCHAR(100) NOT NULL,
@@ -26,7 +27,7 @@ CREATE TABLE Solicitudes (
     IdTipoSolicitud INT NOT NULL,
     Titulo VARCHAR(100) NOT NULL,
     Mensaje TEXT NOT NULL,
-    Archivo BLOB,
+    RutaArchivo VARCHAR(255), -- Cambiado de BLOB a VARCHAR
     Fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     Estado ENUM('Sin revisar', 'Revisado') NOT NULL,
     Respuesta TEXT,
@@ -38,56 +39,66 @@ CREATE TABLE Solicitudes (
 /* .............Creacion de procedimientos de usuarios............ */
 
 DELIMITER //
+
 CREATE PROCEDURE crearUsuarios(
     IN p_Nombre VARCHAR(100),
+    IN p_Apellido VARCHAR(100),
     IN p_Cedula VARCHAR(20),
     IN p_Correo VARCHAR(100),
     IN p_Contrasena VARCHAR(100),
     IN p_Rol ENUM('Admin', 'Usuario')
 )
 BEGIN
-    INSERT INTO usuarios (Nombre, Cedula, Correo, Contrasena, Rol)
-    VALUES (p_Nombre, p_Cedula, p_Correo, p_Contrasena, p_Rol);
+    INSERT INTO usuarios (Nombre, Apellido, Cedula, Correo, Contrasena, Rol)
+    VALUES (p_Nombre, p_Apellido, p_Cedula, p_Correo, p_Contrasena, p_Rol);
 END //
+
 DELIMITER ;
 
 DELIMITER //
+
 CREATE PROCEDURE borrarUsuarios(
     IN p_IdUsuario INT
 )
 BEGIN
     DELETE FROM usuarios WHERE IdUsuario = p_IdUsuario;
 END //
+
 DELIMITER ;
 
 DELIMITER //
+
 CREATE PROCEDURE editarUsuarios(
     IN p_IdUsuario INT,
     IN p_Nombre VARCHAR(100),
+    IN p_Apellido VARCHAR(100),
     IN p_Cedula VARCHAR(20),
     IN p_Correo VARCHAR(100),
     IN p_Contrasena VARCHAR(100)
 )
 BEGIN
     UPDATE usuarios
-    SET Nombre = p_Nombre, Cedula = p_Cedula, Correo = p_Correo, Contrasena = p_Contrasena
+    SET Nombre = p_Nombre, Apellido = p_Apellido, Cedula = p_Cedula, Correo = p_Correo, Contrasena = p_Contrasena
     WHERE IdUsuario = p_IdUsuario;
 END //
+
 DELIMITER ;
 
 
+
 /* .............Creacion de procedimientos de solicitudes............ */
+
 DELIMITER //
 CREATE PROCEDURE crearSolicitud(
     IN p_IdUsuario INT,
     IN p_IdTipoSolicitud INT,
     IN p_Titulo VARCHAR(100),
     IN p_Mensaje TEXT,
-    IN p_Archivo BLOB
+    IN p_RutaArchivo VARCHAR(255) -- Cambiado de BLOB a VARCHAR
 )
 BEGIN
-    INSERT INTO Solicitudes (IdUsuario, IdTipoSolicitud, Titulo, Mensaje, Archivo, Estado, Respuesta)
-    VALUES (p_IdUsuario, p_IdTipoSolicitud, p_Titulo, p_Mensaje, p_Archivo, 'Sin revisar', 'No hay respuestas todavia :(');
+    INSERT INTO Solicitudes (IdUsuario, IdTipoSolicitud, Titulo, Mensaje, RutaArchivo, Estado, Respuesta)
+    VALUES (p_IdUsuario, p_IdTipoSolicitud, p_Titulo, p_Mensaje, p_RutaArchivo, 'Sin revisar', 'No hay respuestas todavia :(');
 END //
 DELIMITER ;
 
@@ -105,11 +116,11 @@ CREATE PROCEDURE editarSolicitud(
     IN p_IdSolicitud INT,
     IN p_Titulo VARCHAR(100),
     IN p_Mensaje TEXT,
-    IN p_Archivo BLOB
+    IN p_RutaArchivo VARCHAR(255) -- Cambiado de BLOB a VARCHAR
 )
 BEGIN
     UPDATE Solicitudes
-    SET Titulo = p_Titulo, Mensaje = p_Mensaje, Archivo = p_Archivo
+    SET Titulo = p_Titulo, Mensaje = p_Mensaje, RutaArchivo = p_RutaArchivo
     WHERE IdSolicitud = p_IdSolicitud;
 END //
 DELIMITER ;
@@ -130,9 +141,10 @@ DELIMITER ;
 /* .............Creacion de datos de prueba............ */
 
 -- Insertar un nuevo usuario o admin
-INSERT INTO Usuarios (Nombre, Cedula, Correo, Contrasena, Rol)
-VALUES ('Juan', '1234567890', 'juan@example.com', 'contraseña', 'Usuario'),
- ('Admin1', '0987654321', 'admin@example.com', 'contraseña', 'Admin');
+INSERT INTO Usuarios (Nombre, Apellido, Cedula, Correo, Contrasena, Rol)
+VALUES ('Juan', 'Perez', '1234567890', 'juan@example.com', 'contrasena', 'Usuario'),
+       ('Administrador', 'Gonzalez', '0987654321', 'admin@example.com', 'contrasena', 'Admin');
+
  
  -- Insertar tipos de solicitud
 INSERT INTO TipoSolicitud (tipo) VALUES
@@ -143,5 +155,6 @@ INSERT INTO TipoSolicitud (tipo) VALUES
 ('Felicitaciones');
 
 -- Insertar una solicitud de ejemplo
-INSERT INTO Solicitudes (IdUsuario, IdTipoSolicitud, Titulo, Mensaje, Archivo, Estado)
+
+INSERT INTO Solicitudes (IdUsuario, IdTipoSolicitud, Titulo, Mensaje, RutaArchivo, Estado)
 VALUES (1, 2, 'Problema de Conexion', 'Hola, recientemente he experimentado algunos problemas de conexion', NULL, 'Sin revisar');
